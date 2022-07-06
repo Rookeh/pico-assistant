48b5e939186e69fc6aed29378f16b85b3a369e48
Rookeh <s.pascoe89@gmail.com>
2022-07-07 00:14:38 +0100

    Update secrets.py

--- src/secrets.py
+++ src/secrets.py
@@ -1,3 +1,5 @@
+# Enter your WiFi details here for the Pico to connect with.
 ssid = "YourWifi"
 password = "YourPassword"
+# Generate a long lived Home Assistant access token and paste it here. (see: https://developers.home-assistant.io/docs/auth_api/#long-lived-access-token)
 ha_access_token = "asdfghjkl1234567890"
