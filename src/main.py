ea0d6275bddd391bc5a5ec2132eadcf5f263ecd0
Rookeh <s.pascoe89@gmail.com>
2022-07-08 22:42:47 +0100

    Changes for camera support
    * Added new display mode which shows just camera image + area name.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,13 @@ from api import HomeAssistant
 import config
 from display import Display
 from pimoroni import Button, RGBLED
+import utils
 import utime
+
+class ViewMode:
+    NONE = 0
+    DEVICES = 1
+    CAMERA = 2
 
 class App:
     
@@ -17,18 +23,41 @@ class App:
         self.led.set_rgb(0,0,0)
         self.currentArea = 0
         self.devices = None
+        self.viewMode = ViewMode.NONE
         self.display = Display()        
         self.display.clear()
         self.display.drawBackground("Please wait...")
 
     def changeArea(self):
+        self.viewMode = ViewMode.NONE
         areaLength = len(list(self.areas.values()))
         if self.currentArea >= 0 and self.currentArea < areaLength - 1:
             self.currentArea += 1
         else:
             self.currentArea = 0
+        self.refreshArea()
+              
+    def refreshCamera(self, entityId):
+        self.viewMode = ViewMode.CAMERA        
+        self.display.clear()        
+        cameraName = list(self.areas.keys())[self.currentArea]
+        #imageBytes = self.haApi.getCameraImageBytes(entityId)
+        #self.display.renderCameraBytes(cameraName, imageBytes)
+        imagePath = self.haApi.getCameraImage(entityId)
+        self.display.renderCamera(cameraName, imagePath)
+        
+            
+    def refreshArea(self):
+        devices = list(self.areas.values())[self.currentArea]
+        predicate = lambda device: device["entity_id"].startswith("camera.")
+        cameraEntity = utils.firstOrDefault(devices, predicate)
+        if cameraEntity is not None:
+            self.refreshCamera(cameraEntity["entity_id"])
+        else:
+            self.refreshDevices()        
     
     def refreshDevices(self):
+        self.viewMode = ViewMode.DEVICES        
         self.display.clear()
         area = list(self.areas.values())[self.currentArea]
         areaName = list(self.areas.keys())[self.currentArea]
@@ -61,20 +90,24 @@ while True:
         app.sleep()
         if app.buttonA.read() or app.buttonB.read() or app.buttonX.read() or app.buttonY.read():
             lastUse = utime.ticks_ms()
-            app.refreshDevices()
+            app.display.wake()
+            app.refreshArea()
     else:
-        if app.buttonA.read() and len(app.devices) >= 1:
-            lastUse = utime.ticks_ms()
-            app.toggleDevice("A", 0)
-        if app.buttonB.read() and len(app.devices) >= 2:
-            lastUse = utime.ticks_ms()
-            app.toggleDevice("B", 1)
-        if app.buttonX.read() and len(app.devices) == 3:
-            lastUse = utime.ticks_ms()
-            app.toggleDevice("X", 2)
-        if app.buttonY.read():
-            lastUse = utime.ticks_ms()
-            app.changeArea()
-            app.refreshDevices()
-    
+        if app.viewMode == ViewMode.CAMERA:
+            if app.buttonY.read():
+                lastUse = utime.ticks_ms()
+                app.changeArea()
+        elif app.viewMode == ViewMode.DEVICES:            
+            if app.buttonA.read() and len(app.devices) >= 1:
+                lastUse = utime.ticks_ms()
+                app.toggleDevice("A", 0)
+            if app.buttonB.read() and len(app.devices) >= 2:
+                lastUse = utime.ticks_ms()
+                app.toggleDevice("B", 1)
+            if app.buttonX.read() and len(app.devices) == 3:
+                lastUse = utime.ticks_ms()
+                app.toggleDevice("X", 2)
+            if app.buttonY.read():
+                lastUse = utime.ticks_ms()
+                app.changeArea()
     utime.sleep(0.1)
