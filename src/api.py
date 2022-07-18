ad84ac3fea240ef8afa1ca11601f7eaed311e9cd
Rookeh <s.pascoe89@gmail.com>
2022-07-18 22:15:12 +0100

    Return byte objects for camera images...
    ...instead of storing on flash.

--- src/api.py
+++ src/api.py
@@ -2,7 +2,6 @@ import config
 import os
 import gc
 import secrets
-import time
 #import upip
 import urequests
 import utils
@@ -19,30 +18,12 @@ class HomeAssistant:
         # Comment out line 7 until installed. Re-comment line 6 once installed.
         #upip.install("micropython-urequests")
         
-    def getCameraImage(self, camera_entity):
+    def getCameraImageBytes(self, camera_entity):             
         response = self.apiRequest("/api/camera_proxy/" + camera_entity)
-        if response.status_code == 200 and len(response.content) > 0:
-            fileName = camera_entity + ".jpg"
-            if utils.fileExists(fileName):
-                os.remove(fileName)
-            imgFile = open(fileName, "wb")
-            written = imgFile.write(response.content)
-            imgFile.close
-            if written > 0:
-                return fileName
-            else:
-                print("failed to write!")
-                return None
+        if response.status_code == 200:
+            return response.content
         else:
             return None
-
-# TODO: Passing bytes directly to JPEGDEC seems to cause a lockup: https://github.com/pimoroni/pimoroni-pico/issues/435
-#    def getCameraImageBytes(self, camera_entity):
-#        response = self.apiRequest("/api/camera_proxy/" + camera_entity)
-#        if response.status_code == 200:
-#            return response.content
-#        else:
-#            return None        
         
     def getDevices(self, devices):
         for device in devices:
